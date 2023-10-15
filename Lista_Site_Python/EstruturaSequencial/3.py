c4fb48b304ce8147fd2aaa3747e2c4a503be6b72
PC - CASA <luiz.gustavolopes@ufrpe.br>
2023-10-14 22:31:36 -0300

    primeiro commit

---
+++ Lista_Site_Python/EstruturaSequencial/3.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+x = float(input('digite o primeiro numero '))
+y = float(input('digite o segundo numero '))
+
+z = x + y
+
+print('a soma é: ', z)
---
+++ Lista_Site_Python/EstruturaSequencial/4.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+n1 = float(input('digite a primeira nota:'))
+n2 = float(input('digite a segunda nota: '))
+n3 = float(input('digite a terceira nota: '))
+n4 = float(input('digite a quarta nota: '))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+print('a media é: ', media)
