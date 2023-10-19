8d6b85736713adc6a9f1146bba9b1b98deb1c29c
Luiz.Gustavo <luizgustavolb2021@gmail.com>
2023-10-19 10:50:48 -0300

    fazendo algumas atividades

--- atvs_faculdade/periodoPassado/2_lista/atv6.py
+++ atvs_faculdade/periodoPassado/2_lista/atv6.py
@@ -4,8 +4,9 @@ n2 = float(input('digite a segunda nota: '))
 md = (n1 + n2) / 2
 
 if md >= 7:
-    print('aprovado')
-elif md > 10:
-    print('aprovado com distinção')
+    if md == 10:
+        print('aprovado com distinção')
+    else:
+        print('aprovado')
 else:
-    print('Reprovado')
+    print('reprovado')
